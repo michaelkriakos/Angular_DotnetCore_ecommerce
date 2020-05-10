8dcadf08cae4cf94d2aa8eb01f178d73c8d747ce
mikelkiko <mikelkiko@yahoo.com>
2020-05-10 12:57:01 -0500

    ui added

--- API/Helpers/Pagination.cs
+++ API/Helpers/Pagination.cs
@@ -4,17 +4,17 @@ namespace API.Helpers
 {
     public class Pagination<T> where T:class
     {
-        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> myProperty)
+        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> d)
         {
             PageIndex = pageIndex;
             PageSize = pageSize;
             Count = count;
-            MyProperty = myProperty;
+            data = d;
         }
 
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
         public int Count { get; set; }  
-        public IReadOnlyList<T> MyProperty { get; set; }
+        public IReadOnlyList<T> data { get; set; }
     }
 }
