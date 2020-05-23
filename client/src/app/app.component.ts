4b670d0f55defb1e810ab6e55efe1d77271beeb9
mikelkiko <mikelkiko@yahoo.com>
2020-05-23 15:35:53 -0500

    add paging-filter-sorting-searching

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products:any[];
-  constructor(private  http: HttpClient) {
-
-  }
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: any) => {
-      this.products=response.data;
-       console.log(response);
-    }, error => {
-      console.log(error);
-    });
-  }
-
+  constructor() {}
+  ngOnInit(): void {}
 }
