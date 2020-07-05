3b7310cdb9059c150012a7b7a463161570702dc5
mikelkiko <mikelkiko@yahoo.com>
2020-07-05 18:30:07 -0500

    add Identity

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            if(!userManager.Users.Any())
+            {
+                var user=new AppUser
+                {
+   DisplayName="Bob",
+               Email="bob@test.com",
+               UserName="bob@test.com",
+               Address=new Address
+               {
+                   FirstName="bob",
+                   LastName="bobbity",
+                   Street="10 the street",
+                   City="New York",
+                   State="NY",
+                   Zipcode="90210"
+               }
+                };
+            await userManager.CreateAsync(user,"mk@1234.COM");
+            }
+        }
+    }
+}
